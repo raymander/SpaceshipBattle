b378e48962b4da259f891898f289393b9c87fd6f
raymander <raychern@yandex.com>
2018-11-05 17:39:55 +0200

    fix enemyscript

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -9,22 +9,26 @@ public class EnemyScript : MonoBehaviour {
     // Use this for initialization
     void Start () {
        
-        StartCoroutine("Move");
+        //StartCoroutine("Move");
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.Translate(Vector3.right * Time.deltaTime);
+        transform.Translate(-Vector3.right * 0.7f * Time.deltaTime);
 
-        transform.LookAt(target.transform);
+        //transform.LookAt(target.transform);
+
+        Vector3 newPos = new Vector3(-transform.position.x, target.transform.position.y, -transform.position.z);
+
+        transform.LookAt(newPos);
     }
 
-    IEnumerator Move() {
-        while (true) {
-            yield return new WaitForSeconds(8f);
-            transform.eulerAngles += new Vector3(0, 180f, 0);
-        }
-    }
+    //IEnumerator Move() {
+    //    while (true) {
+    //        yield return new WaitForSeconds(8f);
+    //        transform.eulerAngles += new Vector3(0, 180f, 0);
+    //    }
+    //}
 }
